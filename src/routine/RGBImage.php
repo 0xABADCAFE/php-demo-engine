7912f8ec9c253887e5a0ab7f027b45144ddef096
0xABADCAFE <karlchurchill@gmail.com>
2021-04-18 01:09:50 +0100

    Optimised the render output of the double vertical RGB mode. * There is a glitch in one of the output cases (where only the background colour of a cell with different foreground changes).

--- src/routine/RGBImage.php
+++ src/routine/RGBImage.php
@@ -21,6 +21,7 @@ declare(strict_types=1);
 namespace ABadCafe\PDE\Routine;
 
 use ABadCafe\PDE;
+use \SPLFixedArray;
 
 /**
  * Display an image
@@ -30,6 +31,10 @@ use ABadCafe\PDE;
 class RGBImage extends Base implements IResourceLoader {
 
     use TResourceLoader;
+
+    private int $iWidth, $iHeight, $iViewWidth, $iViewHeight;
+
+    private SPLFixedArray $oPixels;
 
     const DEFAULT_PARAMETERS = [
         'sPath' => 'required'
@@ -44,8 +49,10 @@ class RGBImage extends Base implements IResourceLoader {
      * @inheritDoc
      */
     public function setDisplay(PDE\IDisplay $oDisplay) : self {
-        $this->bCanRender = ($oDisplay instanceof PDE\Display\IPixelled);
-        $this->oDisplay   = $oDisplay;
+        $this->bCanRender  = ($oDisplay instanceof PDE\Display\IPixelled);
+        $this->oDisplay    = $oDisplay;
+        $this->iViewWidth  = $oDisplay->getWidth();
+        $this->iViewHeight = $oDisplay->getHeight();
         return $this;
     }
 
@@ -54,6 +61,12 @@ class RGBImage extends Base implements IResourceLoader {
      */
     public function render(int $iFrameNumber, float $fTimeIndex) : self {
         if ($this->canRender($iFrameNumber, $fTimeIndex)) {
+            $oBuffer = $this->oDisplay->getPixelBuffer();
+            if ($this->iWidth == $this->iViewWidth && $this->iHeight == $this->iViewHeight) {
+                foreach ($oBuffer as $i => $iBufferRGB) {
+                    $oBuffer[$i] = $this->oPixels[$i];
+                }
+            }
         }
         return $this;
     }
@@ -70,11 +83,20 @@ class RGBImage extends Base implements IResourceLoader {
     protected function loadPNM(string $sPath) {
         $sRaw = $this->loadFile($sPath);
         if (preg_match('/^(\d+)\s+(\d+)$/m', $sRaw, $aMatches)) {
-            $iWidth  = (int)$aMatches[1];
-            $iHeight = (int)$aMatches[2];
-            $sData   = substr($sRaw, ($iWidth * $iHeight * -3));
-            printf("%d x %d, %d\n", $iWidth, $iHeight, strlen($sData));
-            exit();
+            $this->iWidth  = (int)$aMatches[1];
+            $this->iHeight = (int)$aMatches[2];
+            $iArea         = $this->iWidth * $this->iHeight;
+            $this->oPixels = new SPLFixedArray($iArea);
+            $sData         = substr($sRaw, ($iArea * -3));
+            $iDataOffset   = 0;
+            for ($i = 0; $i < $iArea; ++$i) {
+                $this->oPixels[$i] =
+                    (ord($sData[$iDataOffset++]) << 16) |
+                    (ord($sData[$iDataOffset++]) << 8) |
+                    (ord($sData[$iDataOffset++]));
+            }
+        } else {
+            throw new \Exception('Invalid PNM Format');
         }
     }
 

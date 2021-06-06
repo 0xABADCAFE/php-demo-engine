04691a783dc1e94bd966295894fe0e36b15be5d1
0xABADCAFE <karlchurchill@gmail.com>
2021-06-06 20:42:51 +0100

    Reworked pattern structure to be sparse for significant memory savings.

--- src/audio/Pattern.php
+++ src/audio/Pattern.php
@@ -53,7 +53,13 @@ class NoteOff extends Event {
 
 
 /**
- * Basic Pattern block
+ * Pattern
+ *
+ * Basic pattern block. Implements a sparse representation of the event data using regular PHP arrays. There is one
+ * array per channel that is indexed by line number. Only line numbers that have events have an entry.
+ *
+ * Requested lines are returned as fixed length SPLFixedArray representations by combining the events (if any) in each
+ * of the channel arrays for that line number.
  */
 class Pattern {
 
@@ -62,20 +68,28 @@ class Pattern {
         $iNumLines      = 64
     ;
 
-    private array $aEvents = [];
+    private array $aChannels;
 
+    private SPLFixedArray $oRow;
+
+    /**
+     * Constructor. Expects a number of channels and lines.
+     *
+     * @param int $iNumChannels
+     * @param int $iNumLines
+     */
     public function __construct(int $iNumChannels, int $iNumLines) {
         $this->iNumChannels = max(1, $iNumChannels);
         $this->iNumLines    = max(1, $iNumLines);
-        $oRow = new SPLFixedArray($this->iNumChannels);
-        $this->oRows = new SPLFixedArray($this->iNumLines);
-        for ($i = 0; $i < $this->iNumLines; ++$i) {
-            $this->oRows[$i] = clone $oRow;
-        }
+
+        $this->oRow         = new SPLFixedArray($this->iNumChannels);
+        $this->aChannels    = array_fill(0, $this->iNumChannels, []);
     }
 
     /**
-     * Number of channels in this pattern
+     * Number of channels in this pattern,
+     *
+     * :return int
      */
     public function getNumChannels() : int {
         return $this->iChannels;
@@ -83,6 +97,8 @@ class Pattern {
 
     /**
      * Length of the pattern in lines
+     *
+     * @return int
      */
     public function getLength() : int {
         return $this->iNumLines;
@@ -90,20 +106,50 @@ class Pattern {
 
     /**
      * Return the enumerated line
+     *
+     * @param  int $iLineNumber
+     * @return SPLFixedArray
      */
     public function getLine(int $iLineNumber) : SPLFixedArray {
-        return $this->oRows[$iLineNumber];
+        $oRow = clone $this->oRow;
+        foreach ($this->aChannels as $i => &$aChannelEvents) {
+            $oRow[$i] = $aChannelEvents[$iLineNumber] ?? null;
+        }
+        return $oRow;
     }
 
-    public function addEvent(Event $oEvent, int $iChannel, int $iLineNumber, int $iEvery = 0) {
+    /**
+     * Fluently add an event to the pattern at the specified channel and line number. This can also be set to repeat.
+     *
+     * @param  Event $oEvent
+     * @param  int   $iChannel    - Which channel to add the event to
+     * @param  int   $iLineNumber - Which line number to add the event on
+     * @param  int   $iEvery      - Repeat every N lines (0 for no repeat)
+     * @param  int   $iUntil      - Repeat until line ... (0 for end of block)
+     * @return self
+     * @throws \OutOfBoundsException
+     */
+    public function addEvent(Event $oEvent, int $iChannel, int $iLineNumber, int $iEvery = 0, $iUntil = 0) {
+        if (
+            $iChannel < 0    || $iChannel    >= $this->iNumChannels ||
+            $iLineNumber < 0 || $iLineNumber >= $this->iNumLines
+        ) {
+            throw new \OutOfBoundsException();
+        }
+
+        $aChannelEvents = &$this->aChannels[$iChannel];
         if ($iEvery > 0) {
-            while ($iLineNumber < $this->iNumLines) {
-                $this->oRows[$iLineNumber][$iChannel] = $oEvent;
+            if ($iUntil <= 0 || $iUntil > $this->iNumLines) {
+                $iUntil = $this->iNumLines;
+            }
+            while ($iLineNumber < $iUntil) {
+                $aChannelEvents[$iLineNumber] = $oEvent;
                 $iLineNumber += $iEvery;
             }
         } else {
-            $this->oRows[$iLineNumber][$iChannel] = $oEvent;
+            $aChannelEvents[$iLineNumber] = $oEvent;
         }
+        return $this;
     }
 }
 

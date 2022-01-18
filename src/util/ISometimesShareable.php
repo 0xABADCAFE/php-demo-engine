e0b6c14c7db9110b403c0b205731c4a9a9e7ddba
0xABADCAFE <karlchurchill@gmail.com>
2022-01-18 18:55:35 +0000

    - Added interface and default mixins for objects that can be shared idempotently. This is initially for the audio subsystem but applies generally. - Renamed the chip tune machine into something more imaginative.

---
+++ src/util/ISometimesShareable.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *                   ______                            __
+ *           __     /\\\\\\\\_                        /\\\
+ *          /\\\  /\\\//////\\\_                      \/\\\
+ *        /\\\//  \///     \//\\\    ________       ___\/\\\         _______
+ *      /\\\//               /\\\   /\\\\\\\\\_    /\\\\\\\\\       /\\\\\\\\_
+ *    /\\\//_              /\\\\/   /\\\/////\\\   /\\\////\\\     /\\\/////\\\
+ *    \////\\\ __          /\\\/    \/\\\   \/\\\  \/\\\  \/\\\    /\\\\\\\\\\\
+ *        \////\\\ __      \///_     \/\\\___\/\\\  \/\\\__\/\\\   \//\\\//////_
+ *            \////\\\       /\\\     \/\\\\\\\\\\   \//\\\\\\\\\    \//\\\\\\\\\
+ *                \///       \///      \/\\\//////     \/////////      \/////////
+ *                                      \/\\\
+ *                                       \///
+ *
+ *                         /P(?:ointless|ortable|HP) Demo Engine/
+ */
+
+declare(strict_types=1);
+
+namespace ABadCafe\PDE\Util;
+/**
+ * Interface for classes that abstracts away the semantics of whether or not an instance is sharable. To be sharable,
+ * an instance is generally required to be stateless or at least idempotent.
+ *
+ */
+interface ISometimesShareable {
+
+    /**
+     * Returns an instance. This may or may not be the thing being called depending on whether or not it's sharable.
+     */
+    public function share(): self;
+
+}
---
+++ src/util/TNeverShareable.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *                   ______                            __
+ *           __     /\\\\\\\\_                        /\\\
+ *          /\\\  /\\\//////\\\_                      \/\\\
+ *        /\\\//  \///     \//\\\    ________       ___\/\\\         _______
+ *      /\\\//               /\\\   /\\\\\\\\\_    /\\\\\\\\\       /\\\\\\\\_
+ *    /\\\//_              /\\\\/   /\\\/////\\\   /\\\////\\\     /\\\/////\\\
+ *    \////\\\ __          /\\\/    \/\\\   \/\\\  \/\\\  \/\\\    /\\\\\\\\\\\
+ *        \////\\\ __      \///_     \/\\\___\/\\\  \/\\\__\/\\\   \//\\\//////_
+ *            \////\\\       /\\\     \/\\\\\\\\\\   \//\\\\\\\\\    \//\\\\\\\\\
+ *                \///       \///      \/\\\//////     \/////////      \/////////
+ *                                      \/\\\
+ *                                       \///
+ *
+ *                         /P(?:ointless|ortable|HP) Demo Engine/
+ */
+
+declare(strict_types=1);
+
+namespace ABadCafe\PDE\Util;
+
+/**
+ * TNeverShareable
+ *
+ * Mixin for ISometimesSharable implementors that always want copy semantics.
+ */
+trait TNeverShareable {
+
+    /**
+     * @return self
+     */
+    public function share(): self {
+        return clone $this;
+    }
+}
